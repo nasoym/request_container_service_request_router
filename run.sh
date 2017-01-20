8147dd49ff8e4a79bd4a8452863c7a11b2bbf53b
Sinan Goo <sinan.goo@hypoport.de>
2017-01-20 21:31:27 +0100

    variable renaming

--- run.sh
+++ run.sh
@@ -2,18 +2,18 @@
 set -ef -o pipefail
 
 while getopts "p:v" options; do case $options in
-  v) VERBOSE_OPTIONS="-vv" ;;
+  v) SOCAT_OPTIONS="-vv" ;;
   p) PORT="$OPTARG" ;;
 esac; done; shift $(( OPTIND - 1 ))
 
 : ${PORT:="9090"}
-: ${VERBOSE_OPTIONS:=""}
+: ${SOCAT_OPTIONS:=""}
 : ${SERVICE:="$(dirname $0)/service.sh"}
 
 socat_listen_command="TCP-LISTEN:${PORT},reuseaddr,fork"
 
 socat \
-  $VERBOSE_OPTIONS \
+  $SOCAT_OPTIONS \
   $socat_listen_command \
   EXEC:"${SERVICE}"
 
